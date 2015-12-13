5b2cb63c52d2783e8239c3d1372a78a40a030fb6
Petr Vomáčko <p.vomacko@gmail.com>
2015-12-13 23:42:32 +0100

    Final

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ var Twig = require('twig');
 var url = require('url');
 var express = require('express');
 var bodyParser = require('body-parser');
+var async = require('async');
 
 var connection = mysql.createConnection({
     host: '127.0.0.1',
@@ -19,12 +20,16 @@ var connection = mysql.createConnection({
 
 var $App = $App || {};
 
+$App.testQuery = "SELECT * from tests";
+
 //$App.logged = (localStorage.getItem('logged') == true);
-$App.logged = true;
-//$App.user_name = false;
-$App.user_name = 'admin';
+$App.logged = false;
+//$App.logged = true;
+$App.user_name = false;
+//$App.user_name = 'pica';
+$App.user_id = false;
 $App.full_name = false;
-$App.autologout = 15 * 60 * 1000;
+$App.autologout = 15 * 60 * 1000 * 1000;
 $App.logout = false;
 
 var app = express();
@@ -62,7 +67,6 @@ app.get('/delete-user', function (req, response, next) {
         if (err) {
             console.log(err);
         } else {
-            log('deleted?' + $q);
             $q = "SELECT * FROM users WHERE nick != 'admin'";
             query = connection.query($q, function (err, rows) {
                 if (err) {
@@ -70,6 +74,63 @@ app.get('/delete-user', function (req, response, next) {
                 } else {
                     $App.render(response, 'users-list', {"rows": rows});
                 }
+            });
+        }
+    });
+});
+app.get('/delete-result', function (req, response, next) {
+    var $q = "DELETE FROM results WHERE id_user = '" + req.query.id_user + "' AND id_test= '" + req.query.id_test + "'";
+    var query = connection.query($q, function (err, rows) {
+        if (err) {
+            console.log(err);
+        } else {
+            var $q = "SELECT name,points as vysledek,splnen, fullname, id_test, id_user  FROM tests left join results using(id_test) left join users using(id_user)";
+            var query = connection.query($q, function (err, rows) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    $App.render(response, 'results', {"tests": rows});
+                }
+            });
+        }
+    });
+});
+
+
+app.get('/delete-test', function (req, response, next) {
+    var $q = "DELETE FROM tests WHERE id_test = '" + req.query.id_test + "'";
+    $App.helper = [];
+    var query = connection.query($q, function (err, rows) {
+        if (err) {
+            console.log(err);
+        } else {
+            $q = "SELECT * FROM questions WHERE id_test = '" + req.query.id_test + "'";
+            query = connection.query($q, function (err, rows) {
+                if (err) {
+                    console.log(err);
+                }
+                else {
+                    for ($i in rows) {
+                        var $idd = rows[$i].id_question;
+                        $App.helper.push($idd);
+                        query = connection.query("DELETE FROM answer where id_question = '" + $idd + "'", function (err, rows) {
+                            if (err) {
+                                log(err)
+                            } else {
+                                var $q = "DELETE FROM questions WHERE id_question = '" + $App.helper.pop() + "'";
+                                query = connection.query($q, function (arr, rows) {
+                                    if (arr) {
+                                        log(arr)
+                                    }
+                                });
+                            }
+                        });
+                    }
+                }
+            });
+            connection.query($App.testQuery, function (error, result) {
+                if (error)console.log($quest, error);
+                else $App.render(response, 'tests-list', {"tests": result});
             });
         }
     });
@@ -93,7 +154,18 @@ app.post('/add-user', function (req, response, next) {
 });
 
 app.get('/tests', function (req, response, next) {
-    $App.render(response, 'tests-list');
+    var $tests;
+    var $done;
+
+    connection.query($App.testQuery, function (error, result) {
+
+        $tests = result;
+        $done = "SELECT * FROM `results` where id_user = " + $App.user_id;
+        connection.query($done, function (error, ress) {
+            $done = ress;
+            $App.render(response, 'tests-list', {"tests": $tests,"done":$done});
+        });
+    });
 });
 
 app.get('/tests-add', function (req, response, next) {
@@ -107,16 +179,14 @@ $App.render = function (res, page, params) {
         if ($App.logged) {
             params = params || {};
             params.username = $App.user_name;
+            params.userid = $App.user_id;
             res.render(page + '.twig', params);
         } else {
-            res.render('_denied.twig');
+            res.render('login.twig');
         }
     }
 };
 
-//app.get('/username', function (req, response, next) {
-//    response.send($App.user_name);
-//});
 app.get('/login-form', function (req, response, next) {
     $App.render(response, 'login');
 });
@@ -145,6 +215,18 @@ app.get('/users-list', function (req, response, next) {
         }
     });
 });
+app.get('/results', function (req, response, next) {
+    if(typeof req.query.id_user != 'undefined'){
+        var $q = "SELECT name,points as vysledek,splnen, fullname, id_test, id_user  FROM tests left join results using(id_test) left join users using(id_user) WHERE id_user = '"+req.query.id_user+"'";
+    }else $q = "SELECT name,points as vysledek,splnen, fullname, id_test, id_user  FROM tests left join results using(id_test) left join users using(id_user)";
+    var query = connection.query($q, function (err, rows) {
+        if (err) {
+            console.log(err);
+        } else {
+            $App.render(response, 'results', {"tests": rows});
+        }
+    });
+});
 
 app.post('/login', function (req, response, next) {
     var $q = "SELECT * FROM users WHERE nick = '" + req.body.login_name + "' and pw = md5('" + req.body.login_pw + "')";
@@ -157,7 +239,12 @@ app.post('/login', function (req, response, next) {
                 console.log('User logged: ' + rows[0].nick + '=>' + rows[0].fullname);
                 $App.logged = true;
                 $App.user_name = rows[0].nick;
+                $App.user_id = rows[0].id_user;
                 $App.full_name = rows[0].fullname;
+
+                //$App.testQuery = "select id_test, results.id_user, name as nazev, count(question_name) as otazek,sum(questions.points) as bodu " +
+                //    "from tests left join results using(id_test) left join questions using(id_test) where results.id_user != "+$App.user_id+" OR results.id_user is NULL group by id_test";
+                $App.testQuery = "select id_test,name as nazev, count(question_name) as otazek,sum(questions.points) as bodu from tests left join questions using(id_test) group by id_test";
                 //localStorage.setItem('logged', true);
 
                 $App.render(response, 'index');
@@ -168,16 +255,213 @@ app.post('/login', function (req, response, next) {
                 }, $App.autologout);
             } else {
                 console.log('Failed: ' + $q);
-                response.send('false');
+                $App.render(response,'login');
             }
         }
     });
 });
 
+app.post('/create-test', function (req, response, next) {
+    $App.helper = [];
+    //console.log(req.body);
+    var $testname = req.body.test_name;
+
+    $test = "INSERT INTO tests (`id_test`, `name`) VALUES (NULL, '" + $testname + "')";
+    connection.query($test, function (err, result) {
+        if (err)console.log($test, err); else {
+            var $idtest = parseInt(result.insertId);
+            for (var param in req.body) {
+                if (param.indexOf('question') != -1) {
+                    $question = req.body[param];
+                    $id = param.substr(param.indexOf('question') + 8);
+                    $App.helper.push($id);
+                    $quest = "INSERT INTO questions (`id_question`, `question_name`, `points`, `id_test`) VALUES (NULL, '" + req.body[param] + "', " + parseInt(req.body['points' + $id]) + ", " + $idtest + ")";
+
+                    connection.query($quest, function (error, res) {
+                        if (error)console.log($quest, error); else {
+                            $id = $App.helper.shift();
+
+                            var $id_question = parseInt(res.insertId);
+                            for (var i = 0; i <= 3; i++) {
+                                if (typeof req.body['answer' + $id + '-' + i] !== 'undefined') {
+                                    var $answer = req.body['answer' + $id + '-' + i];
+                                    if (typeof req.body['right' + $id + '-' + i] !== 'undefined') {
+                                        var $answ = "INSERT INTO answer (`id_answer`, `id_question`, `answer`, `correct`) VALUES (NULL, " + $id_question + ", '" + $answer + "', 'true');";
+                                    } else {
+                                        $answ = "INSERT INTO answer (`id_answer`, `id_question`, `answer`, `correct`) VALUES (NULL, " + $id_question + ", '" + $answer + "', 'false');";
+                                    }
+                                    connection.query($answ, function (error_answ, response_answ) {
+                                        if (error_answ)console.log($quest, error_answ);
+                                    });
+                                } else {
+                                    i = 4;
+                                }
+                            }
+                        }
+                    });
+                }
+            }
+            connection.query($App.testQuery, function (error, result) {
+                if (error)console.log($quest, error);
+                else $App.render(response, 'tests-list', {"tests": result});
+            });
+        }
+    });
+});
+
+app.get('/view-test', function (req, response, next) {
+    var $id_test = req.query.id_test;
+    var $points = 0;
+
+    $render = "select id_test, name as nazev,count(question_name) as otazek,sum(points) as bodu from tests left join questions using(id_test) where id_test = '" + $id_test + "' group by id_test";
+    connection.query($render, function (error, result) {
+        $points = result[0].bodu;
+    });
+
+    $render = "SELECT * FROM tests left join questions using(id_test) left join answer using(id_question) where id_test = '" + $id_test + "'";
+    var $query = connection.query($render, function (error, result) {
+        $App.render(response, 'tests-view', {'rows': result, 'points': $points});
+    });
+});
+
+app.get('/results-list', function (req, response, next) {
+    var $user = req.query.user;
+    var $test = req.query.test;
+
+    $render = "select sum(points) as bodu from tests left join questions using(id_test) where id_test = '" + $test + "' group by id_test";
+
+    var $test_res = '';
+
+    connection.query($render, function (error, result) {
+        $test_res = result;
+
+        $query = "select * from results where id_test = '" + $test + "' and id_user = '"+$user+"' group by id_test";
+        connection.query($query,function (error, result) {
+
+            var $done = result[0].splnen == 'true';
+
+            $App.render(response, 'tests-result', {'testresult': '<p>Počet bodů: ' + result[0].points + '</p><p>Test ' + ($done ? 'byl splněn.' : 'nebyl splněn.') + '</p>'});
+        });
+
+    });
+});
+
+app.get('/start-test', function (req, response, next) {
+    var $id_test = req.query.id_test;
+    var $points = 0;
+
+    $render = "select id_test, name as nazev, count(question_name) as otazek,sum(points) as bodu from tests left join questions using(id_test) where id_test = '" + $id_test + "' group by id_test";
+    connection.query($render, function (error, result) {
+        $points = result[0].bodu;
+    });
+
+    $render = "SELECT * FROM tests left join questions using(id_test) left join answer using(id_question) where id_test = '" + $id_test + "'";
+    var $query = connection.query($render, function (error, result) {
+        $App.render(response, 'tests-view', {'rows': result, 'points': $points});
+    });
+});
+
 app.get('/logout', function (req, response, next) {
     $App.logged = false;
-    $App.render(response,'login');
+    $App.render(response, 'login');
 });
+
+//app.get('/expired-test', function (req, response, next) {
+//    $App.render(response, 'error-page',{error: 'Časový limit vypršel <button onclick="window.location = \'/testslist\';" class="btn-basic">Zpět na přehled testů</button>'});
+//});
+
+app.post('/submit-test', function (req, response, next) {
+    var $id_test = req.body.id_test;
+
+    $render = "SELECT * FROM tests left join questions using(id_test) left join answer using(id_question) where id_test = '" + $id_test + "'";
+    var $query = connection.query($render, function (error, result) {
+        var $points = 0;
+        var $skip = false;
+        var $right = 0;
+        var $current = result[0];
+        var $fine = 0;
+        var $answered = false;
+
+
+        for ($i in result) {
+            $elem = result[$i];
+
+
+            if ($current.id_question != $elem.id_question) {
+                if (!$answered) {
+                    $skip = $elem.id_question;
+                    log('otazka nezodpovezena: ' + $current.question_name);
+                } else {
+                    if ($fine) {
+                        log('V poradku: ' + $current.question_name);
+                        $points += $current.points;
+                        $right++;
+                    } else {
+                        log('Spatne: ' + $current.question_name);
+                    }
+                }
+                $current = $elem;
+                $skip = false;
+                $fine = false;
+                $answered = false;
+            }
+
+            if ($skip != $elem.id_question) {
+                $skip = false;
+                $fine = true;
+                var $found = false;
+                //log('--------- Searching: ' + $elem.answer);
+                for (var param in req.body) {
+                    if (param.indexOf('check') != -1) {
+                        $question = param;
+                        $id_question = $question.substring(5, $question.indexOf('-'));
+                        $id_answ = $question.substring($question.indexOf('-') + 1);
+
+                        if ($id_question == $elem.id_question) {
+                            $answered = true;
+                            if ($elem.id_answer == $id_answ) {
+                                $found = true;
+                                $fine = $elem.correct == 'true';
+                                //log($fine + ' -> ' + $elem.answer);
+                                if (!$fine)$skip = $elem.id_question;
+                            }
+
+                        }
+                    }
+                }
+                if(!$found && $elem.correct == 'true'){
+                    $fine = false;
+                    $skip = $elem.id_question;
+                }
+
+            }
+        }
+
+        if (!$answered) {
+            log('otazka nezodpovezena: ' + $current.question_name);
+        } else {
+            if ($fine) {
+                log('V poradku: ' + $current.question_name);
+                $points += $current.points;
+            } else {
+                log('Spatne: ' + $current.question_name);
+            }
+        }
+
+        $render = "select id_test, name as nazev, count(question_name) as otazek,sum(points) as bodu from tests left join questions using(id_test) where id_test = '" + $id_test + "' group by id_test";
+        connection.query($render, function (error, result) {
+            var $done = (($points / parseInt(result[0].bodu)) * 100) >= 60;
+            log('Pocet bodu: ' + $points + '/' + result[0].bodu + ' {' + $done + '}');
+
+            $result = "INSERT INTO `silar`.`results` (`id_result`, `id_user`, `id_test`, `points`,`splnen`) VALUES (NULL, ?, ?, ?, ?)";
+            connection.query($result,[$App.user_id,$id_test,($points + '/' + result[0].bodu),$done ? 'true' : 'false'], function (error, result) {});
+
+            $App.render(response, 'tests-result', {'testresult': '<p>Počet bodů: ' + $points + '/' + result[0].bodu + '</p><p>Test ' + ($done ? 'byl splněn.' : 'nebyl splněn.') + '</p>'});
+        });
+    });
+});
+
+
 
 server.listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));
--- web/js/app.js
+++ web/js/app.js
@@ -6,6 +6,9 @@
  */
 var $App = $App || {};
 
+$App.maxQuestions = 20;
+$App.maxAnswers = 4;
+
 
 $App.getAjaxData = function (target, data) {
     if (typeof(data) === 'undefined') {
@@ -47,6 +50,16 @@ $App.checkbox = function(){
     });
 };
 
+$App.customCheckbox = function(){
+    $('.question-row input:checkbox').each(function(){
+        $(this).customInput();
+    });
+    //$('.testform')[0].reset();
+    if($('#testform').length){
+        document.getElementById("testform").reset();
+    }
+};
+
 $App.testForm = function(){
     $App.checkbox();
     $('.ajax-action').unbind('click').bind('click',function(e){
@@ -58,20 +71,18 @@ $App.testForm = function(){
             var $ans = $id.substr($id.indexOf('-')+1);
             $id = $id.substring($id.indexOf('answer')+6,$id.indexOf('-'));
             $ans++;
-            if($ans < 4) {
-                var $answer = '<div class="add_form__answers answer"><input type="text" placeholder="Odpověď" value="" name="answer' + $id + '-' + $ans + '">' +
-                    '<input class="checkbox" type="checkbox" name="right' + $id + '-' + $ans + '" id="right' + $id + '-' + $ans + '"><label for="right' + $id + '-' + $ans + '">Správná odpověď</label></div>';
-                $($answer).insertBefore(this).hide().fadeIn(500);
-                $App.checkbox();
-                if($ans == 3)
-                    $(this).remove();
-            }
+            var $answer = '<div class="add_form__answers answer"><input type="text" placeholder="Odpověď" value="" name="answer' + $id + '-' + $ans + '">' +
+                '<input class="checkbox" type="checkbox" name="right' + $id + '-' + $ans + '" id="right' + $id + '-' + $ans + '"><label for="right' + $id + '-' + $ans + '">Správná odpověď</label></div>';
+            $($answer).insertBefore(this).hide().fadeIn(500);
+            $App.checkbox();
+            if($ans == ($App.maxAnswers-1))
+                $(this).remove();
         }else{
             $before = $(this).parent().find('.question:last').find('input:first-of-type').attr('name');
             $id = $before.substr($before.indexOf('question')+8);
             $id++;
 
-            var $question = '<div class="add_form__row question"><label>Název otázky</label>' +
+            var $question = '<div class="add_form__row question"><label>Název otázky {'+(parseInt($id)+1)+'}</label>' +
                 '<input type="text" placeholder="Název otázky" value="" name="question'+$id+'"></div><div class="add_form__row">' +
                 '<label>Počet bodů</label><input type="number" placeholder="Počet bodů" value="" name="points'+$id+'"></div>' +
                 '<div class="add_form__answers answer"><input type="text" placeholder="Odpověď" value="" name="answer'+$id+'-0">' +
@@ -80,9 +91,29 @@ $App.testForm = function(){
             '<div class="add_form__splitter">&nbsp;</div>';
             $($question).insertBefore(this).hide().fadeIn(500);
             $App.testForm();
+            if($id == ($App.maxQuestions-1)){
+                $(this).prev().remove();
+                $(this).remove();
+            }
 
         }
         //log($(this).data('action'));
+    });
+};
+
+$App.initSearch = function () {
+    $('#search').keyup(function(){
+        var valThis = $(this).val().toString();
+        valThis = valThis.toLowerCase();
+        $('.tests-list_item:not(.tests-list_item-header), .users-list_item').each(function(){
+            var text = $(this).data('search').toString();
+            text = text.toLowerCase();
+            if(text.indexOf(valThis) != -1){
+                $(this).show();
+            }else{
+                $(this).hide();
+            }
+        });
     });
 };
 
@@ -90,9 +121,88 @@ $App.testForms = function(){
     log($('.add_form').find('form').serialize());
 };
 
+$App.deleteTest = function(){
+    $('.delete-test').click(function(event) {
+        event.preventDefault();
+        var r=confirm("Opravdu chcete smazat tento test?");
+        if (r==true)   {
+            window.location = $(this).attr('href');
+        }
+    });
+    $('.delete-user').click(function(event) {
+        event.preventDefault();
+        var r=confirm("Opravdu chcete smazat tohoto uživatele?");
+        if (r==true)   {
+            window.location = $(this).attr('href');
+        }
+    });
+    $('.delete-result').click(function(event) {
+        event.preventDefault();
+        var r=confirm("Opravdu chcete smazat tento výsledek testu?");
+        if (r==true)   {
+            window.location = $(this).attr('href');
+        }
+    });
+};
+
+$App.hideTime = function(){
+
+    if($('.hide-time').length) {
+
+        $('.hide-time').click(function () {
+            if ($(this).is('.rotated')) {
+                $(this).removeClass('rotated');
+                $('.remaining-time').removeClass('behind');
+            } else {
+                $(this).addClass('rotated');
+                $('.remaining-time').addClass('behind');
+            }
+        });
+
+        $App.testTime = parseInt($('.timer').html());
+
+        $('.timer').html($App.testTime + ':00');
+        $App.testTime = $App.testTime * 60;
+
+        setTimeout(function () {
+            decreaseTime();
+        }, 1000);
+    }
+};
+
+function decreaseTime(){
+    $App.testTime = $App.testTime-1;
+    var minutes = parseInt($App.testTime/60);
+    var seconds = $App.testTime%60;
+
+    if($App.testTime == 0){
+        var $name = $('input[name="id_test"]').val();
+        window.location = '/expired-test?id_test='+$name;
+    }else {
+        //log(minutes + ':' + seconds);
+        $('.timer').html(minutes + ':' + seconds);
+
+        if(minutes < 1){
+            if(seconds == 59){
+                $('.remaining-time').addClass('warning');
+            }
+            $('.hide-time').removeClass('rotated');
+            $('.remaining-time').removeClass('behind');
+        }
+
+        setTimeout(function () {
+            decreaseTime();
+        }, 1000);
+    }
+}
+
 $App.init = function(){
     $App.resize();
     $App.testForm();
+    $App.initSearch();
+    $App.deleteTest();
+    $App.hideTime();
+    $App.customCheckbox();
     $('.btn-test').click(function(e){
         e.preventDefault();
     });
